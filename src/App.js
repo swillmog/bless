75aeb736cf83f41cf739e7ec130e98fab6aac768
lanre <williams.sodiq79@gmail.com>
2024-02-17 20:26:04 +0000

    another commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
 
 
 function App() {
   return (
-    <div className="App">
-
-      <Router>
-        <Navbar />
+    <Router>
         <Routes>
-        <Route path="/" exact component={Home} />
-        </Routes>
+        <Route path="/" >
+          <Route index element = {<> <Navbar /> < Home /> <Footer /></> } />
+          <Route path="Menu"  element = {<> <Navbar /> < Menu /> <Footer /></> } />
+          <Route path="About"  element = {<> <Navbar /> < About /> <Footer /></> } />
+          <Route path="Contact"  element = {<> <Navbar /> < Contact /> <Footer /></> } />
+        </Route>
+             </Routes>
       </Router>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "../styles/Footer.css";
 
 
 function Footer() {
   return (
     <div className="footer">
         <div className="socialMedia">
-      <InstagramIcon/>
+      <InstagramIcon/> <TwitterIcon/> <FacebookIcon/> <LinkedInIcon/>
         </div>
         <p> &copy; 2024 www.cyberwilliams.com </p>
     </div>
